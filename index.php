7062bd00ca2d32f3bb029963b8b009c86a7cd2f3
Christian Zamora <christiancreizi@hotmail.com>
2017-03-08 15:16:47 -0600

    Anexo de nuevo archivo
    por Christian Zamora

---
+++ index.php
@@ -0,0 +1,308 @@
+<!Doctype>
+<html lang="es" xml:lang="es" xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+	<title>Equipo Medico - Venta de Equipo Medico</title>
+	<link type="image/x-icon" href="images/siv_medical_sivmedical_varfra_logo.ico" rel="icon" />
+	<link rel="stylesheet" href="css/estilos.css">
+	<link rel="stylesheet" href="fonts/fonts.css">
+	<meta name="keywords" content="Equipos, Medicos, Equipos Medicos, Equipo Medico, Reparacion de Equipo Medico, Reparacion de Equipos Medicos,  Mantenimiento de Equipo Medico,  Mantenimiento de Equipos Medicos, Accesorios de Equipos Medicos, Accesorios de Equipo Medico, Servicios de Equipo Medico,  Servicios de Equipos Medicos, Refacciones de Equipos Medicos,  Refacciones de Equipo Medico, Consumibles de Equipo Medico, Consumibles de Equipos Medicos, Software Medico, Software de Equipos Medicos">
+	<meta name="copyright" content="Varfra">
+	<meta name="description" content="Equipo Medico VARFRA">
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"><meta name="distribution" content="global">
+	<link rel="stylesheet" type="text/css" href="css/Master_index.css">
+	<script src="js/jquery-1.10.2.js" type="text/javascript"></script>
+	<script src="js/jquery.animate-colors.js" type="text/javascript"></script>
+	<script type="text/javascript" src="js/effect.js"></script>
+	<script type="text/javascript" src="js/slide.js"></script>
+	<script type="text/javascript" src="js/drop.js"></script>
+	<script type="text/javascript" src="js/banner.js"></script>
+	<script type="text/javascript" src="js/Master.js"></script>
+<!-- Inserta esta etiqueta en la sección "head" o justo antes de la etiqueta "body" de cierre. -->
+<script src="https://apis.google.com/js/platform.js" async defer>
+  {lang: 'es'}
+</script>
+<script src="https://apis.google.com/js/platform.js" async defer>
+  {lang: 'es'}
+</script>
+</head>
+<body onload="banner()">
+<div class="social">
+		<ul>
+			<li><a href="https://www.facebook.com/sivmedico/" target="_blank" class="icon-facebook"></a></li>
+			<li><a href="https://twitter.com/multimediavarf1" target="_blank" class="icon-twitter"></a></li>
+			<li><a href="https://www.youtube.com/channel/UCYB_5FtmQj6CQqkUnpOKu_A" target="_blank" class="icon-youtube"></a></li>
+			<li><a href="https://www.instagram.com/sivmedical/" target="_blank" class="icon-instagram"></a></li>
+			<li><a href="https://www.linkedin.com/in/sivmedical-varfra-ab7b034a?trk=nav_responsive_tab_profile" target="_blank" class="icon-linkedin2"></a></li>
+			<li><a href="contacto_equipo_medico.php" class="icon-mail"></a></li>
+			<li><a class="g-plusone"></a></li>
+		</ul>
+	</div>
+
+	<?php include_once("analyticstracking.php") ?>
+	<header>
+		<div id="mainHeader">
+			<div id="headerH">
+				<div id="contentLogoHeader"><a href="index.php"><img src="images/equipo_medico_logo.png"/></a></div>
+				<hgroup><h2><strong style="color: #0B3861;">Soluciones en <br> Equipos Médicos</strong></h2></hgroup>
+				<div id="headSearch">
+				<form id="searchH" action="busqueda_equipo_medico.php" method="post">
+				<input type="search" placeholder="Busqueda..." name="busqueda" 
+				style="height:18px; width:100px; font-size:12px; font-family:'segoeuil';'" required>
+				<input type="submit" value="Buscar" style="font-size:12px;"></form>
+				</div>
+			</div>
+			<a class="btnMibew" href="/mibew/client.php?locale=en&amp;style=silver" target="_blank" onclick="if(navigator.userAgent.toLowerCase().indexOf('opera') != -1 &amp;&amp; window.event.preventDefault) window.event.preventDefault();this.newWindow = window.open(&#039;/mibew/client.php?locale=en&amp;style=silver&amp;url=&#039;+escape(document.location.href)+&#039;&amp;referrer=&#039;+escape(document.referrer), 'mibew', 'toolbar=0,scrollbars=0,location=0,status=1,menubar=0,width=640,height=480,resizable=1');this.newWindow.focus();this.newWindow.opener=window;return false;"><img src="http://varfra.com/mibew/b.php?i=mblue&amp;lang=es" border="0" width="177" height="61" alt=""/></a><!-- / mibew button --> 
+			<div id="navHeader">
+				<nav>
+					<ul>
+						<li><a href="index.php">Inicio</a></li>
+						<li><a href="categoria/acero_inoxidable_equipo_medico.php">Equipos Medicos</a></li>
+						<li><a href="servicios2_equipo_medico.php">Servicio</a>
+							<ul class="navSubMenu">
+								<li><a href="capacitacion_equipo_medico.php">Capacitación</a></li>
+								<li><a href="cursos_equipo_medico.php">Cursos</a></li>
+								<li><a href="logistica_equipo_medico.php">Logística</a></li>
+								<li><a href="mantenimiento_equipo_medico.php">Mantenimiento</a></li>
+								<li><a href="personalizacion_equipo_medico.php">Personalización</a></li>
+								<li><a href="renta_equipo_medico.php">Renta</a></li>
+								<li><a href="reparacion_equipo_medico.php">Reparación</a></li>
+								<li><a href="ventas_equipo_medico.php">Ventas</a></li>
+								<li><a onclick="direction(1)">Lista de Servicios</a></li>
+							</ul>
+						</li>
+						<li><a href="marcas_equipo_medico.php">Marcas</a></li>
+						
+						<li><a href="componentes_equipo_medico.php">Componentes y Accesorios</a></li>
+						<li><a href="ofertas_menu.php">Ofertas</a></li>
+						<li><a class="anavAbsolute" href="contacto_equipo_medico.php">Contacto</a></li>
+					</ul>
+				</nav>
+			</div>
+		</div>
+	</header>
+
+<!-----------------------------Body-------------------->
+<div id="wrap">
+	<div id="main">
+		<div id="content">
+		<div id="divDireccion">
+				<p><a href="Tel: 5555455021"><strong style="color: #0B3861;">  Llame al Teléfono: </strong> <strong style="color: #936;">(55) 55 45 50 21 (CDMX) con 20 lineas Activas.. </strong> </a> <a href="Tel: 018005235549"><strong style="color: #0B3861;">  Llama Gratis al Teléfono </strong><strong style="color: #936;"> 01 800 523 5549 </strong></a>
+					<br><a href="Tel: 5532697358"><strong style="color: #0B3861;">  Envía un WhatsApp:</strong><strong style="color: #936;">  5532697358 </strong></a>
+					<!--<br>Fax.: 52 55 21 59 EXT. 102<br><br>-->
+				</p>
+				<h2><strong style="color: #0B3861;"></strong></b></h2> <h2><strong style="color: #0B3861;">Somos una Empresa Dedicada al Servicio de Todo en Equipo Médico, Visita Nuestro Catálogo en Línea, Hacemos Envios a toda la República Mexicana y al Extranjero. Envianos tu Cotización ó Contáctanos por Chat en Línea</strong></h2>
+			</div>
+			<div id="contentAside">
+				<div id="bodyAside">
+					<a id="tituloAsideIndex">Categorías</a>
+					<ul id="ulAsideIndex">
+						<li class="liAsideIndex"><a title="Venta de Equipo Médico Nuevo y Remanufacturado">Catálogo de Equipo médico <span style="text-align:right; float:right;">></span></a>
+							<ul class="ul_Sm_AsideIndex">
+								<?php
+									include_once 'conexion.php';
+
+									function fn_Unaccent($str_To_Convert)
+									{
+										$arr_Accent = array('á','é','í','ó','ú');
+										$arr_Unaccent = array('a','e','i','o','u');
+										return str_replace($arr_Accent, $arr_Unaccent, $str_To_Convert);
+									}
+
+									$str_Qry_AsideEquipo = "select idcategoria, categoria, cate_file from sivCategoria order by categoria";
+									$qry_AsideEquipo = mysql_query($str_Qry_AsideEquipo);
+									$int_Counter =1;
+									while($fch_AsideEquipo = mysql_fetch_row($qry_AsideEquipo))
+									{
+										?>
+										<li class="liAsideIndex"><a title="Venta de <?php echo $fch_AsideEquipo[1];?>" href="categoria/<?php echo $fch_AsideEquipo[2];?>">
+												<?php echo $fch_AsideEquipo[1] ?> </a>
+												</li>
+										<?php
+									}
+								?>
+							</ul>
+
+						</li>
+						<li class="liAsideIndex"><a title="Accesorios de Equipo Médico" href="categoria/acero_inoxidable_equipo_medico.php">Accesorios</a></li>
+						<li class="liAsideIndex"><a title="Asesoría y Capacitación en Equipo Medico" href="cursos_equipo_medico.php">Asesoría y Capacitación</a></li>
+						<li class="liAsideIndex"><a title="Bibliografías de Equipo Médico" href="servicio/venta_de_manuales_equipos_medicos.php">						Bibliografías</a></li>
+						<li class="liAsideIndex"><a title="Redes de Comunicación en Hospitales">				Comunicaciones<span style="text-align:right; float:right;">></span></a>
+							<ul class="ul_Sm_AsideIndexsm">
+								<li class="liAsideIndex"><a title="Mantenimiento Correctivo a Equipo Médico" href="servicio/fibra_optica_equipo_medico.php">Fibra Óptica</a></li>
+								<li class="liAsideIndex"><a title="Mantenimiento Correctivo a Equipo Médico" href="servicio/telefonia_voip_equipo_medico.php">Telefonía Voip</a></li>
+								<li class="liAsideIndex"><a title="Mantenimiento Correctivo a Equipo Médico" href="servicio/torre_comunicacion_equipo_medico.php">Torres de Comunicación</a></li>
+							</ul>
+						</li>
+						<li class="liAsideIndex"><a title="Construcción de Laboratorios y Hospitales" href="servicio/construccion_equipo_medico.php">Construcción y Arquitectura</a></li>
+						<li class="liAsideIndex"><a title="Equipo Industrial Hospitalario" href="servicio/equipo_industrial_hospitalario.php">Equipo Industrial Hospitalario</a></li>
+						<li class="liAsideIndex"><a title="Exportación de Equipo Médico" href="servicio/exportacion_equipo_medico.php">						Exportación</a></li>
+						<li class="liAsideIndex"><a title="Importación de Equipo Médico" href="servicio/importacion_equipo_medico.php">						Importación</a></li>
+						<li class="liAsideIndex"><a title="Informatica y redes para Hospitales" href="servicio/informatica_equipo_medico.php">				Informática y Software</a></li>
+						<li class="liAsideIndex"><a title="Instrumental para Laboratorios y Hospitales" href="servicio/instrumental_equipo_medico.php">		Instrumental</a></li>
+						<li class="liAsideIndex"><a title="Insumos y Consumibles para Equipo Medico" href="servicio/insumos_y_consumibles_equipo_medico.php">			Insumos y Consumibles</a></li>
+						<li class="liAsideIndex"><a title="Venta, surtido de Medicamentos y Farmacia" href="servicio/medicamentos_y_farmacia_equipo_medico.php">			Medicamentos y Farmacia</a></li>
+						<li class="liAsideIndex"><a title="Mobiliario Médico para Hospitales" href="servicio/mobiliario_medico_equipo_medico.php">					Mobiliario Médico</a></li>
+						<!--<li class="liAsideIndex"><a title="Nutrición en Hospitales">							Nutrición</a></li>-->
+						<li class="liAsideIndex"><a title="Publicidad y Medios para Hospitales y Laboratorios" href="servicio/publicidad_para_hospitales_equipo_medico.php">	Publicidad y Medios</a></li>
+						<li class="liAsideIndex"><a title="Sistemas de Seguridad para Hospitales y Clinicas">	Seguridad <span style="text-align:right; float:right;">></span></a>
+							<ul class="ul_Sm_AsideIndexsm">
+								<li class="liAsideIndex"><a title="Mantenimiento Correctivo a Equipo Médico" href="servicio/codigos_de_seguridad_equipo_medico.php">Códigos de Seguridad</a></li>
+								<li class="liAsideIndex"><a title="Mantenimiento Correctivo a Equipo Médico" href="servicio/seguridad_policiaca_equipo_medico.php">Seguridad para Instalaciones</a></li>
+								<li class="liAsideIndex"><a title="Mantenimiento Correctivo a Equipo Médico" href="servicio/seguridad_policiaca_equipo_medico.php">Seguridad Policiaca</a></li>
+								<li class="liAsideIndex"><a title="Mantenimiento Correctivo a Equipo Médico" href="servicio/seguridad_informatica_equipo_medico.php">Seguridad en Redes Informáticas</a></li>
+							</ul>							
+						</li>
+						<li class="liAsideIndex"><a title="Venta, Mantenimiento y Reparación de Equipo Médico">	Servicios<span style="text-align:right; float:right;">></span></a>
+							<ul class="ul_Sm_AsideIndexsm">
+								<li class="liAsideIndex"><a title="Mantenimiento Correctivo a Equipo Médico" href="servicio/servicio_enfermeria_equipo_medico.php">Servicios para Enfermería</a></li>
+								<li class="liAsideIndex"><a title="Mantenimiento Correctivo a Equipo Médico" href="servicio/servicios_de_especialidades_medicas_equipo_medico.php">Servicios de Especialidad y de Alta Especialidad</a></li>
+							</ul>
+						</li>
+						<li class="liAsideIndex"><a title="Servicios Integrales para Hospitales" href="servicio/mantenimiento_servicios_integrales_equipo_medico.php">				Servicios Integrales</a></li>
+						<li class="liAsideIndex"><a title="Transporte, envío de Equipo Médico y Renta de Ambulancias" href="servicio/transporte_ambulancias_equipo_medico.php">Transporte y Ambulancias</a></li>
+						<li class="liAsideIndex"><a title="Venta de Uniformes para Hospitales">					Vestuario<span style="text-align:right; float:right;">></span></a>
+							<ul class="ul_Sm_AsideIndexsm">
+								<li class="liAsideIndex"><a title="Mantenimiento Correctivo a Equipo Médico" href="servicio/batas_hospitalarias_equipo_medico.php">Batas para Hospitales</a></li>
+								<li class="liAsideIndex"><a title="Mantenimiento Correctivo a Equipo Médico" href="servicio/gorro_quirurgico_equipo_medico.php">Gorras</a></li>
+								<li class="liAsideIndex"><a title="Mantenimiento Correctivo a Equipo Médico" href="servicio/guantes_esteriles_equipo_medico.php">Guantes</a></li>
+								<li class="liAsideIndex"><a title="Mantenimiento Correctivo a Equipo Médico" href="servicio/uniformes_hospitalarios_equipo_medico.php">Uniformes para Hospitales</a></li>
+								<li class="liAsideIndex"><a title="Mantenimiento Correctivo a Equipo Médico" href="servicio/zapatones_esteriles_equipo_medico.php">Zapatones</a></li>
+							</ul>
+						</li>
+
+
+							<ul class="ul_Sm_AsideIndex">
+								
+							</ul>
+					</ul>
+				</div>
+			</div>
+
+			<section id="sectionBanner">
+				<div id="bodyTitle">
+				<div id="divDescripcion1" class="divDescripcion" style="display:block;">
+					<form id="busqMain" action="busqueda_equipo_medico.php" method="post">
+					<div id="titleNavMain"><p class="tituloDesc" >TENEMOS TODO LO QUE BUSCAS PARA HOSPITALES, CLÍNICAS Y USUARIOS FINALES.</p></div>
+					</form>
+				</div>
+					<div id="banner">
+						<img src="images/main_Varfra_equipo_medico_siv_medical_sivmedical.jpg" name="banner1"/>
+						<a id="tituloBanner" href="javascript:servicios_equipo_medico_fn();"><br><br></a>
+						<ul id="banUl1" class="abannerd">
+							<li>
+								<a href="marcas_equipo_medico.php">¿Porqué somos tu mejor Opción?</a>
+								<a href="marcas_equipo_medico.php">Porque tenemos el mejor Precio.</a>
+								<a href="marcas_equipo_medico.php">Porque resolvemos tus necesidades.</a>
+								<a href="marcas_equipo_medico.php">Porque enviamos a toda la República y al Extranjero.</a>
+							</li>
+						</ul>
+						<ul  id="banUl2" class="abannerd"  class="invisible" style="display:none;">
+							<li>
+								<form action="https://www.paypal.com/cgi-bin/webscr" method="post" style="float: right;">
+                    <input type="hidden" name="business" value="viomdiq@prodigy.net.mx">
+                    <input type="hidden" name="cmd" value="_xclick">
+                    <input type="hidden" name="item_name" value="Gel/Pasta Conductiva Nuprep">
+                    <input type="hidden" name="amount" value="198.00">
+                    <input type="hidden" name="currency_code" value="MXN">
+                    <input type="hidden" name="undefined_quantity" value="1">
+                    <input type="image" src="https://www.paypal.com/es_XC/MX/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal, la forma mÃ¡s segura y rÃ¡pida de pagar en lÃ­nea.">
+                    <img alt="" border="0" src="https://www.paypal.com/es_XC/i/scr/pixel.gif" width="1" height="1">
+                    </form>
+							</li>
+						</ul>
+						<ul id="banUl3" class="abannerd" class="invisible" style="display:none;">
+							<li>
+								<form action="https://www.paypal.com/cgi-bin/webscr" method="post" style="float: right;">
+                    <input type="hidden" name="business" value="viomdiq@prodigy.net.mx">
+                    <input type="hidden" name="cmd" value="_xclick">
+                    <input type="hidden" name="item_name" value="Alicate Weingart">
+                    <input type="hidden" name="amount" value="90.00">
+                    <input type="hidden" name="currency_code" value="MXN">
+                    <input type="hidden" name="undefined_quantity" value="1">
+                    <input type="image" src="https://www.paypal.com/es_XC/MX/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal, la forma mÃ¡s segura y rÃ¡pida de pagar en lÃ­nea.">
+                    <img alt="" border="0" src="https://www.paypal.com/es_XC/i/scr/pixel.gif" width="1" height="1">
+                    </form>
+							</li>
+						</ul>
+				
+					<ul id="banUl4" class="abannerd" class="invisible" style="display:none;">
+							<li>
+								<form action="https://www.paypal.com/cgi-bin/webscr" method="post" style="float: right;">
+                    <input type="hidden" name="business" value="viomdiq@prodigy.net.mx">
+                    <input type="hidden" name="cmd" value="_xclick">
+                    <input type="hidden" name="item_name" value="Placa para Electrocauterio">
+                    <input type="hidden" name="amount" value="1600.00">
+                    <input type="hidden" name="currency_code" value="MXN">
+                    <input type="hidden" name="undefined_quantity" value="1">
+                    <input type="image" src="https://www.paypal.com/es_XC/MX/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal, la forma mÃ¡s segura y rÃ¡pida de pagar en lÃ­nea.">
+                    <img alt="" border="0" src="https://www.paypal.com/es_XC/i/scr/pixel.gif" width="1" height="1">
+                    </form>
+							</li>
+						</ul>
+						
+					</div>
+				</div>
+				
+			</section>
+
+			
+			<div id="contentServices" class="unvisible">
+				<div id="headerServices">
+					<hgroup><a href="javascript:servicios_equipo_medico_fn();"><h3>Clic aquí para ver nuestros Servicios Hospitalarios, a Laboratorios y a Usuarios Finales. <span style="font-size:16px;"> Nuestro servicio hace la diferencia.</span></h3></a></hgroup>
+				</div>
+				<div id="indexMainServices">
+
+					<!----Content services------------>
+				</div>
+			</div>
+			<div id="divDireccion">
+				<p>Lago Belgrano No. 20, Col. Ampliación Granada, Miguel Hidalgo, C.P. 11529
+					<br>Tel. (55) 55 45 50 21 con 20 Lineas Activas.
+					<!--<br>Fax.: 52 55 21 59 EXT. 102<br><br>-->
+				</p>
+			</div>
+		</div>
+	</div>
+</div>
+</body>
+
+<!---------------------------------FOOTER---------->
+<footer>
+
+	<script>
+	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+	  ga('create', 'UA-56363606-1', 'auto');
+	  ga('send', 'pageview');
+
+	</script>
+
+	<div id="mainFooter">
+		<div id="navFooter">
+			<ul>
+				<li>
+					<a href="index.php">Inicio</a>
+					<a href="nuestra_empresa_equipo_medico.php">¿Quienes somos?</a>
+					<a href="nuestros_clientes_equipo_medico.php">Nuestros Clientes</a>
+					<a href="privacidad_varfra.php">Aviso de Privacidad</a>
+					<a href="contacto_equipo_medico.php">Contacto</a>
+				</li>
+			</ul>
+		</div>
+		<!-- Inserta esta etiqueta donde quieras que aparezca Botón +1. -->
+		<div class="g-plusone" data-annotation="inline" data-width="200"></div>
+		<!-- TWITTER -->
+		<a href="https://twitter.com/share" class="twitter-share-button" data-url="TuEquipoMedico" 
+			data-text="Mira las ofertas que tenemos para ti ésta semana en www.todoenequipomedico.com. (Por @TuEquipoMedico)" 
+			data-count="horizontal">Tweet</a>
+		<script type="text/javascript" src="//platform.twitter.com/widgets.js">
+					
+		</script>
+	</div>
+
+</footer>
+</html>
